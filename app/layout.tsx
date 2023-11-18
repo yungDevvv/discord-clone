094910eeeaa7acb7897aeea106a98e3b5a9e3db6
Sem <sem.elmlm@gmail.com>
2023-11-18 14:46:29 +0200

    server modal, servers sidebar, functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from "@/components/providers/theme-provider"
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -21,15 +22,16 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn([
-          font.className, 
+          font.className,
           'h-full bg-white dark:bg-[#313338]'
-          ])}>
-        <ThemeProvider
+        ])}>
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import {
     Dialog,
     DialogContent,
@@ -16,16 +15,16 @@ import {
     FormLabel,
     FormMessage
 } from '@/components/ui/form'
-import {Input} from "@/components/ui/input"
+import { Input } from "@/components/ui/input"
 import { Button } from '@/components/ui/button';
 
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import * as z from "zod"
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
 import { FileUpload } from '@/components/ui/file-upload';
 import axios from 'axios'
 import { useRouter } from 'next/navigation';
+import { useModal } from '@/hooks/use-modal-store';
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -36,10 +35,11 @@ const formSchema = z.object({
     })
 });
 
-export const InitialModal = () => {
-    const [isMounted, setIsMounted] = useState(false);
-
+export const CreateServerModal = () => {
+    const { isOpen, onClose, type } = useModal();
     const router = useRouter();
+
+    const isModalOpen = isOpen && type === "createServer";
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -53,24 +53,21 @@ export const InitialModal = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
-           await axios.post('/api/servers', values);
-            console.log(values)
-           form.reset();
-           router.refresh();
-           window.location.reload();
+            await axios.post('/api/servers', values);
+            form.reset();
+            router.refresh();
+            onClose();
         } catch (error) {
             console.log(error)
         }
     }
-    useEffect(() => {
-        setIsMounted(true);
-    }, [])
-    // fix for hydration error
-    if(!isMounted) {
-        return null;
+
+    const handleClose = () => {
+        form.reset();
+        onClose();
     }
     return (
-        <Dialog open>
+        <Dialog open={isModalOpen} onOpenChange={handleClose}>
             <DialogContent className='bg-white text-black p-0 overflow-hidden'>
                 <DialogHeader className='pt-8 px-6'>
                     <DialogTitle className='text-2xl text-center font-bold'>
@@ -84,36 +81,36 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
                         <div className='space-y-8 px-6'>
                             <div className='flex items-center justify-center text-center'>
-                                <FormField 
-                                control={form.control} 
-                                name="imageUrl" 
-                                render={({field}) => (
-                                    <FormItem>
-                                        <FormControl>
-                                            <FileUpload
-                                                endpoint="serverImage"
-                                                value={field.value}
-                                                onChange={field.onChange}
-                                            />
-                                        </FormControl>
-                                    </FormItem>
-                                )}
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
                                 />
                             </div>
                             <FormField
                                 control={form.control}
                                 name="name"
-                                render={({field}) => (
+                                render={({ field }) => (
                                     <FormItem>
                                         <FormLabel className='uppercase text-xs font-bold dark:text-secondary/70 text-zinc-500'>
                                             Server name
                                         </FormLabel>
                                         <FormControl>
-                                            <Input 
-                                            className='bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0' 
-                                            placeholder='Enter server name'
-                                            {...field}
-                                            disabled={isLoading} />
+                                            <Input
+                                                className='bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0'
+                                                placeholder='Enter server name'
+                                                {...field}
+                                                disabled={isLoading} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
